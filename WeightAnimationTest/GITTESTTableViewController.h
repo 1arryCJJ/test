66aa8d3d9584a865fce7bb73852878e8238cf6e5
1arryCJJ <larrycjj@163.com>
2019-11-06 17:04:58 +0800

    测试的

---
+++ WeightAnimationTest/GITTESTTableViewController.h
@@ -0,0 +1,17 @@
+//
+//  GITTESTTableViewController.h
+//  WeightAnimationTest
+//
+//  Created by Larry on 2019/11/6.
+//  Copyright © 2019 com.chanxa. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface GITTESTTableViewController : UITableViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
